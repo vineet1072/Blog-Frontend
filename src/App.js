555101b6d845e97e85beb369a9d1ba837784fb1c
unknown <vinuchiha26@gmail.com>
2022-10-06 17:02:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/Account/Login";
+import { Box } from "@mui/material";
+import DataProvider from "./context/DataProvider";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/home/Home";
+import { useState } from "react";
+import Header from "./components/header/Header";
+import CreatePost from "./components/create/CreatePost";
+import DetailView from "./components/details/DetailView";
+import Update from "./components/create/Update";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  return isAuthenticated ? (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate replace to="/login" />
+  );
+};
 
 function App() {
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <BrowserRouter>
+        <Box style={{ marginTop: 64 }}>
+          <Routes>
+            <Route
+              path="/login"
+              element={<Login isUserAuthenticated={isUserAuthenticated} />}
+            />
+            <Route
+              path="/"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/" element={<Home />} />
+            </Route>
+
+            <Route
+              path="/create"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/create" element={<CreatePost />} />
+            </Route>
+
+            <Route
+              path="/details/:id"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/details/:id" element={<DetailView />} />
+            </Route>
+
+            <Route
+              path="/update/:id"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/update/:id" element={<Update />} />
+            </Route>
+
+            <Route
+              path="/about"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/about" element={<About />} />
+            </Route>
+
+            <Route
+              path="/contact"
+              element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+            >
+              <Route path="/contact" element={<Contact />} />
+            </Route>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
 
